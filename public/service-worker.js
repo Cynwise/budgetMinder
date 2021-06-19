e1a0a6e47d7dfe8f3b96f821d05afc50c09d5175
William Farnsworth <williamcoreyf@gmail.com>
2021-06-18 21:09:19 -0400

    implemented service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'static-cache-v2';
+const DATA_CACHE_NAME = 'data-cache-v1';
+
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/index.js",
+    "/db.js",
+    "/styles.css"
+];
+
+self.addEventListener("install" , function (evt) {
+    evt.waitUntil(
+        caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
+    );
+
+    evt.waitUntil(
+        chaces.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    );
+
+    self.skipWaiting();
+});
