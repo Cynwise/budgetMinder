90d216df4a7a3bb5c8ff571876ec4b60bfec5bd9
William Farnsworth <williamcoreyf@gmail.com>
2021-06-20 18:21:12 -0400

    more heroku fixes

--- server.js
+++ server.js
@@ -17,11 +17,13 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require('./routes/api.js'));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
