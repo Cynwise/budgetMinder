97a92ce7dc207eaa34983a38e735b2fb6ddefb91
William Farnsworth <williamcoreyf@gmail.com>
2021-06-28 15:05:55 -0400

    added active event listener to service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -10,16 +10,37 @@ const FILES_TO_CACHE = [
     "icons/icon-192x192.png"
 ];
 
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';
+
 self.addEventListener("install" , function (evt) {
     // evt.waitUntil(
     //     caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
     // );
 
     evt.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+        caches.open(PRECACHE).then((cache) => cache.addAll(FILES_TO_CACHE))
     );
 
     self.skipWaiting();
+});
+
+self.addEventListener('active', evt => {
+  const currentCaches = [PRECACHE, RUNTIME];
+  evt.waitUntil(
+    cache.keys()
+    .then(cache => {
+      return cache.filter(cache => !currentCaches.includes(cachesName));
+    })
+    .then(cacheToDelete => {
+      return Promise.all(
+        cacheToDelete.map(cacheToDelete => {
+          return cache.delete(cacheToDelete);
+        })
+      );
+    })
+    .then(() => self.ClientRectList.claim())
+  );
 });
 
 self.addEventListener("fetch", function (evt) {
